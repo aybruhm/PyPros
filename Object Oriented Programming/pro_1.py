a320b5ecfee13a1466a5f422ca2b819736e18003
Israel Abraham <israelvictory87@gmail.com>
2020-06-03 09:38:28 +0100

    Compute the area and perimeter of a circle constructed by a radius using a class name Circle

---
+++ Object Oriented Programming/pro_1.py
@@ -0,0 +1,20 @@
+'''
+Write a Python class named Circle constructed by a radius and 
+two methods which will compute the area and 
+the perimeter of a circle.
+'''
+
+class Circle:
+    
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2*3.14
+
+    def perimeter(self):
+        return 2*self.radius*3.14
+
+circle = Circle()
+print(circle.age())
+print(circle.perimeter())
